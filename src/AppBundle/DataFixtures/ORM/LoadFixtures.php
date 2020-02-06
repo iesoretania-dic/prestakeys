4a72619f4537c468d60550979f29854ed88820ae
Luis Ramón López López <lrlopez@gmail.com>
2020-02-06 23:43:41 +0100

    Codificación de contraseñas en los fixtures
    Ojo: Se ha actualizado la versión de un componente (DoctrineFixturesBundle), es necesario ejecutar "composer install"

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -3,14 +3,29 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
+use AppBundle\Entity\Usuario;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Fixtures;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class LoadFixtures implements FixtureInterface
+class LoadFixtures extends Fixture
 {
+    private $userPasswordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager);
+        $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager, ['providers' => [$this]]);
+    }
+
+    public function codificaClave($clave)
+    {
+        return $this->userPasswordEncoder->encodePassword(new Usuario(), $clave);
     }
 }
