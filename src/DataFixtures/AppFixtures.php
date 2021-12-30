489028516ad5c65281ffbbf3b7c560586221b308
Luis Ramón López <lrlopez@gmail.com>
2021-12-30 23:33:59 +0100

    Creados fixtures de Departamento

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\DepartamentoFactory;
 use App\Factory\EmpleadoFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -10,6 +11,23 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $secretario = EmpleadoFactory::new()->create([
+                                           'nombre' => 'Secretario',
+                                           'apellidos' => 'IES',
+                                           'secretario' => true
+                                       ]);
+        $ordenanza = EmpleadoFactory::new()->create([
+                                           'nombre' => 'Ordenanza',
+                                           'apellidos' => 'IES',
+                                           'ordenanza' => true
+                                       ]);
+
+        EmpleadoFactory::createMany(30);
+
+        DepartamentoFactory::createMany(8, function() {
+            return ['jefatura' => DepartamentoFactory::faker()->boolean(80) ? EmpleadoFactory::random() : null];
+        });
+
         $manager->flush();
     }
 }
---
+++ src/Factory/DepartamentoFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Departamento;
+use App\Repository\DepartamentoRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<Departamento>
+ *
+ * @method static Departamento|Proxy createOne(array $attributes = [])
+ * @method static Departamento[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Departamento|Proxy find(object|array|mixed $criteria)
+ * @method static Departamento|Proxy findOrCreate(array $attributes)
+ * @method static Departamento|Proxy first(string $sortedField = 'id')
+ * @method static Departamento|Proxy last(string $sortedField = 'id')
+ * @method static Departamento|Proxy random(array $attributes = [])
+ * @method static Departamento|Proxy randomOrCreate(array $attributes = [])
+ * @method static Departamento[]|Proxy[] all()
+ * @method static Departamento[]|Proxy[] findBy(array $attributes)
+ * @method static Departamento[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Departamento[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static DepartamentoRepository|RepositoryProxy repository()
+ * @method Departamento|Proxy create(array|callable $attributes = [])
+ */
+final class DepartamentoFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
+            'descripcion' => self::faker()->text(),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+        return $this
+            // ->afterInstantiate(function(Departamento $departamento): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Departamento::class;
+    }
+}
