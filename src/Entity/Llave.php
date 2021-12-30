89c8b4d88b9b465954b6c3ade232de7643da14bf
Luis Ramón López <lrlopez@gmail.com>
2021-12-30 14:29:36 +0100

    Añadido repositorio de la entidad Llave

--- src/Entity/Llave.php
+++ src/Entity/Llave.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
+use App\Repository\LlaveRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass=LlaveRepository::class)
  */
 class Llave
 {
@@ -13,85 +14,61 @@ class Llave
      * @ORM\Id()
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
-     * @var int
+     * @var ?int
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=20)
-     * @var string
+     * @var ?string
      */
     private $codigo;
 
     /**
      * @ORM\Column(type="string", length=60)
-     * @var string
+     * @var ?string
      */
     private $descripcion;
 
     /**
      * @ORM\Column(type="boolean")
-     * @var bool
+     * @var ?bool
      */
     private $disponible;
 
-    /**
-     * @return int
-     */
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
-    public function getCodigo(): string
+    public function getCodigo(): ?string
     {
         return $this->codigo;
     }
 
-    /**
-     * @param string $codigo
-     * @return Llave
-     */
-    public function setCodigo(string $codigo): Llave
+    public function setCodigo(string $codigo): self
     {
         $this->codigo = $codigo;
         return $this;
     }
 
-    /**
-     * @return string
-     */
-    public function getDescripcion(): string
+    public function getDescripcion(): ?string
     {
         return $this->descripcion;
     }
 
-    /**
-     * @param string $descripcion
-     * @return Llave
-     */
-    public function setDescripcion(string $descripcion): Llave
+    public function setDescripcion(string $descripcion): self
     {
         $this->descripcion = $descripcion;
         return $this;
     }
 
-    /**
-     * @return bool
-     */
-    public function isDisponible(): bool
+    public function isDisponible(): ?bool
     {
         return $this->disponible;
     }
 
-    /**
-     * @param bool $disponible
-     * @return Llave
-     */
-    public function setDisponible(bool $disponible): Llave
+    public function setDisponible(bool $disponible): self
     {
         $this->disponible = $disponible;
         return $this;
---
+++ src/Repository/LlaveRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Departamento;
+use App\Entity\Llave;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Departamento|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Departamento|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Departamento[]    findAll()
+ * @method Departamento[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LlaveRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Llave::class);
+    }
+
+}
