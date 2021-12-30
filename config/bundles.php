7d3d2c04526da21cc34d6c33c48ffef8f051db76
Luis Ramón López <lrlopez@gmail.com>
2021-12-30 23:23:48 +0100

    Instalados componentes de fixtures, incluyendo Foundry
    composer req orm-fixtures --dev
    composer require zenstruck/foundry --dev

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
