caee826f5fd5710abe4839f442368216a2de78f9
Luis Ramon Lopez Lopez <lrlopez@gmail.com>
2022-03-08 10:49:34 +0100

    Añadida configuración de autenticación

--- src/Controller/SeguridadController.php
+++ src/Controller/SeguridadController.php
@@ -6,15 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SeguridadController extends AbstractController
 {
     /**
      * @Route("/entrar", name="seguridad_login")
      */
-    public function entrar() : Response
+    public function entrar(AuthenticationUtils $authenticationUtils) : Response
     {
-        return $this->render('seguridad/entrada.html.twig');
+        return $this->render('seguridad/entrada.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+            'ultimo_usuario' => $authenticationUtils->getLastUsername()
+        ]);
     }
 
     /**
